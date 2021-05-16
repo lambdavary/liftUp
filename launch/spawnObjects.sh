553239b72e2c38f56f3dfb6e4963fa9581e15c5d
lambdavary <yilmaz.sanli@agu.edu.tr>
2021-05-16 23:50:59 +0300

    Tanker and yau models updated

--- launch/spawnObjects.sh
+++ launch/spawnObjects.sh
@@ -1,4 +1,2 @@
-rosrun gazebo_ros spawn_model -file ../gazeboModels/cessna1.urdf -urdf -model yau -x 0 -y 0 -z 0.5
-#rosrun gazebo_ros spawn_model -file ../gazeboModels/tanker.sdf -sdf -model tanker -x 4 -y 0 -z 1
-rosrun gazebo_ros spawn_model -file /home/lambdavary/model_editor_models/tanker/model.sdf -sdf -model tanker -x 4 -y 0 -z 1
-
+rosrun gazebo_ros spawn_model -file ../gazeboModels/tanker.sdf -sdf -model tanker -x 6 -y 0 -z 1
+rosrun gazebo_ros spawn_model -file ../gazeboModels/yau.sdf -sdf -model yau -x 0 -y 0 -z 0.5
